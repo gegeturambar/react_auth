0f61ce43c1101ea2a20a8bd2f53827c25a55ecca
jeresimonney <gegeturambar@gmail.com>
2018-02-01 16:30:30 +0100

    end first step

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,4 +1,21 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { browserHistory, Router, BrowserRouter } from 'react-router';
+import routes from './routes.js';
 
-ReactDom.render(<h1>hello from React</h1>, document.getElementById('react-app'));
+injectTapEventPlugin();
+
+//ReactDom.render(<h1>hello from React</h1>, document.getElementById('react-app'));
+
+ReactDom.render(
+    (
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+      <Router history={browserHistory} routes={routes} />
+    </MuiThemeProvider>
+    ), 
+    document.getElementById('react-app')
+);
+
